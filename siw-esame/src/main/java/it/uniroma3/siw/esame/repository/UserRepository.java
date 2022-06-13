19243d5605651f32d74f03000ff700b8aade5a30
IC-99 <92325157+IC-99@users.noreply.github.com>
2022-06-13 18:07:17 +0200

    reset

--- siw-esame/src/main/java/it/uniroma3/siw/esame/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.siw.esame.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.uniroma3.siw.esame.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
